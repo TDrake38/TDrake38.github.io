bea0684cf92a949c272113c41b381efa88bf752f
TDrake38 <thomasdrake_38@hotmail.com>
2021-05-22 15:21:51 -0600

    set up contact page, added js, added login

---
+++ js/main.js
@@ -0,0 +1,35 @@
+console.log()
+
+$(document).ready(function () {
+    $('.button').click(function (event) {
+        console.log('click works')
+
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>Email is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid</div>')
+        }
+
+        if(name.length >= 3){
+            statusElm.append('<div>Name valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Name not valid</div>')
+        }
+
+        if(message.length >= 10){
+            statusElm.append('<div>Message valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message not valid</div>')
+        }
+        
+    })
+})
