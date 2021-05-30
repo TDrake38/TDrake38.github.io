2cdce8b8f514ec55624b2827bc1cb10383b07f15
TDrake38 <thomasdrake_38@hotmail.com>
2021-05-29 18:58:01 -0600

    set js to cons log the contact us/register page

--- js/main.js
+++ js/main.js
@@ -1,35 +1,10 @@
-console.log()
+addEventListener('submit', runEvent);
 
-$(document).ready(function () {
-    $('.button').click(function (event) {
-        console.log('click works')
+function runEvent(e){
+    e.preventDefault();
 
-        var name = $('.name').val()
-        var email = $('.email').val()
-        var message = $('.message').val()
-        var statusElm = $('.status')
-        statusElm.empty()
+    console.log(document.querySelector('.name').value);
+    console.log(document.querySelector('.email').value);
+    console.log(document.querySelector('.message').value);
+}
 
-        if(email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusElm.append('<div>Email is valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>Email is not valid</div>')
-        }
-
-        if(name.length >= 3){
-            statusElm.append('<div>Name valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>Name not valid</div>')
-        }
-
-        if(message.length >= 10){
-            statusElm.append('<div>Message valid</div>')
-        } else {
-            event.preventDefault()
-            statusElm.append('<div>Message not valid</div>')
-        }
-        
-    })
-})
--- js/register.js
+++ js/register.js
@@ -1,1 +1,9 @@
-console.log()
+addEventListener('submit', regEvent);
+
+function regEvent(e){
+    e.preventDefault();
+
+    console.log(document.querySelector('#rUsername').value);
+    console.log(document.querySelector('#rEmail').value);
+    console.log(document.querySelector('#setPassword').value);
+}
