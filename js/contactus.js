e39de03aeca76e5bb813d4b5693605f8f87e5e25
TDrake38 <thomasdrake_38@hotmail.com>
2021-07-28 11:00:47 -0230

    removed login/r-emerg/r--contact, contact from works,

--- js/contactus.js
+++ js/contactus.js
@@ -1,10 +1,35 @@
 //This is for the contact us page.
-document.addEventListener('submit', runEvent);
 
-function runEvent(e){
-    e.preventDefault();
+$(document).ready(function () {
+    $('.button').click(function (event) {
+        console.log('click works')
 
-    console.log(document.querySelector('.name').value);
-    console.log(document.querySelector('.email').value);
-    console.log(document.querySelector('.message').value);
-};
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>Email is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid</div>')
+        }
+
+        if(name.length >= 3){
+            statusElm.append('<div>Name valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Name not valid</div>')
+        }
+
+        if(message.length >= 10){
+            statusElm.append('<div>Message valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message not valid</div>')
+        }
+        
+    })
+})
