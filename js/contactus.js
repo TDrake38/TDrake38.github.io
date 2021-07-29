d42de612a84997d0d6d7ed0d8b194f81bf40be49
TDrake38 <thomasdrake_38@hotmail.com>
2021-07-29 14:15:10 -0230

    fixed spelling, added icon, edited error messages

--- js/contactus.js
+++ js/contactus.js
@@ -11,24 +11,24 @@ $(document).ready(function () {
         statusElm.empty()
 
         if(email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusElm.append('<div>Email is valid</div>')
+            statusElm.append('<div>Email valid.</div>')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Email is not valid</div>')
+            statusElm.append('<div>Email is not valid: Must be 5 characters long with an @ and a period.</div>')
         }
 
         if(name.length >= 3){
-            statusElm.append('<div>Name valid</div>')
+            statusElm.append('<div>Name valid.</div>')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Name not valid</div>')
+            statusElm.append('<div>Name not valid: Must be atleast 3 characters long.</div>')
         }
 
         if(message.length >= 10){
-            statusElm.append('<div>Message valid</div>')
+            statusElm.append('<div>Message valid.</div>')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Message not valid</div>')
+            statusElm.append('<div>Message not valid: Must be atleast 10 characters long.</div>')
         }
         
     })
